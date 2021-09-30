9c56ffc64ba9b29cd5626d06b5e305f0e1d7b772
wellinton <wellingtonnascimento3@outlook.com>
2021-09-30 01:21:29 -0300

    test

--- index.js
+++ index.js
@@ -8,12 +8,20 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Bem vindo");
 });
 
 app.get("/index", (req, res) => {
   res.render("index");
 });
+
+app.get("/cadastro", (req, res) => {
+    res.render("cadastro");
+  });
+
+  app.get("/detalhes", (req, res) => {
+    res.render("detalhes");
+  });
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
